5d4426564eedcaf4e46001923ec57ac32e9ba905
Anıl Torun <burak.olgu@hotmail.com>
2022-04-30 21:15:02 +0300

    MVC Kamp 2-4
    MVC Kamp Projesi sertifika görevi 2

---
+++ AnimeYazilim/BusinessLayer/Abstract/IWriterService.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using EntityLayer.Dextra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IWriterService
+    {
+        List<Writer> GetList();
+        void WriterAdd(Writer writer);
+        Writer GetById(int id);
+        void WriterDelete(Writer writer);
+        void WriterUpdate(Writer writer);
+        List<WriterExtra> GetListExtra();
+    }
+}
